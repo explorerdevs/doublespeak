00569ad5c26a6e2f69635e43f0c7fb8b2f5d1805
Prince Muel <vansomecsam@gmail.com>
2024-04-10 02:57:09 +0100

    feat: make sure tailwind is working

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@ import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
-  return <h1>Home Page</h1>;
+  return <h1 class="text-2xl text-purple-600">Home Page</h1>;
 });
 
 export const head: DocumentHead = {
